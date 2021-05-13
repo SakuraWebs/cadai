5c3db969438f175177c98bef3ab27d230eb4d9f4
efempreendimentos <enrique.rfm@gmail.com>
2021-05-13 11:15:47 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-var nome="Enrique R. Fernández";
+var nome="Enrique Fernández";
 var cargo = "CTO - Growdev";
 
 var nomeHtml = document.getElementById("nome-no-html");
 var cargoHtml = document.getElementById("cargo-no-html");
-var texto1 = document.getElementById("texto-01");
-var texto2 = document.getElementById("texto-02");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
 
 
 function colocarNomeNoHteml(nome){
@@ -15,17 +15,19 @@ function colocarCargoNoHtml(cargo){
     cargoHtml.innerHTML = cargo;
 }
 
+function clickNoProjetos(){
+    console.log("Clicou no botão projetos");
+    texto2.style.display="block";
+    texto1.style.display="none";
+}
 
-function logarNome(){
-    console.log(nome);
+function clickNoSobre(){
+    console.log("Clicou no botão sobre");
+    texto1.style.display="block";
+    texto2.style.display="none";
+}
 
-    }
 
-  function clickNoProjetos(){
-      console.log("Clicou no botão projetos");
-      texto2.style.display="block";
-  }
-    
 colocarNomeNoHteml(nome);
 colocarCargoNoHtml(cargo);
 
